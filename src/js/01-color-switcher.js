6607d12ed61bc22d4c1f2e6614aa18b51b324f73
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2023-10-13 23:32:30 +0300

    add color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,6 +8,9 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
+startButton.addEventListener('click', startChangingColor);
+stopButton.addEventListener('click', stopChangingColor);
+
 function startChangingColor() {
 startButton.disabled = true; 
 intervalId = setInterval(() => {
@@ -20,5 +23,4 @@ startButton.disabled = false;
 clearInterval(intervalId);
 }
 
-startButton.addEventListener('click', startChangingColor);
-stopButton.addEventListener('click', stopChangingColor);
+
