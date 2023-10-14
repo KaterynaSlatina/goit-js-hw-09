70c414be3bb8f9d02fac80b3ee64de55d065d10a
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2023-10-14 19:24:48 +0300

    add hw

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('form.form'),
+  step: document.querySelector('[name="step"]'),
+  delay: document.querySelector('[name="delay"]'),
+  amount: document.querySelector('[name="amount"]'),
+}
+
+refs.form.addEventListener('click', onPromiceCreate);
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((position, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+    
+  });
+  
+}
+
+
+function onPromiceCreate(evt) {
+  evt.preventDefault();
+
+  let valueDelay = Number(refs.delay.value);
+  let valueStep = Number(refs.step.value);
+  let valueAmount = Number(refs.amount.value);
+
+  for (let i = 1; i <= valueAmount; i += 1) {
+    let promoceDelay = valueDelay + valueStep * i;
   }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 }
